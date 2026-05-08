d96ef8a7c184288d6ccc94f6a28520eef2382428
Rui Nelson <7100905+RuiNelson@users.noreply.github.com>
2026-05-08 18:05:55 +0100

    refactor(DataPipe): replace Array queue with Deque from swift-collections
    Add swift-collections dependency and use Deque<Package> instead of
    [Package] for O(1) popFirst. Fix deinit semaphore balancing to guard
    wait return value and break on failure instead of ignoring it.

--- Package.swift
+++ Package.swift
@@ -26,6 +26,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/RuiNelson/PathWorks.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.0"),
     ],
     targets: [
         .target(
@@ -60,6 +61,7 @@ let package = Package(
             dependencies: [
                 "libsmb2",
                 .product(name: "PathWorks", package: "PathWorks"),
+                .product(name: "Collections", package: "swift-collections"),
             ],
         ),
         .testTarget(
